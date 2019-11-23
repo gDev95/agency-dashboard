7907bce0ddc2023b6c7073d9512f7334fdd9eb1c
gDev95 <paulmeyerber@gmail.com>
2019-11-24 00:12:27 +0100

    env variables for api

--- src/shared/apollo/client.ts
+++ src/shared/apollo/client.ts
@@ -6,7 +6,7 @@ const cache = new InMemoryCache();
 
 const client = new ApolloClient({
 	cache,
-	link: new HttpLink({ uri: "http://localhost:9000/graphql" })
+	link: new HttpLink({ uri: `${process.env.REACT_APP_BASE_API_URL}/graphql` })
 });
 
 export default client;
