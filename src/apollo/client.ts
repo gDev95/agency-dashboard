33163d134350072fa78bb082176472067da901bd
Paul Meyer <paul.meyer@nexenio.com>
2021-02-06 16:19:00 +0100

    Add static json and configure proxy for agency api

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -3,8 +3,9 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 
 const cache = new InMemoryCache();
-
+console.log("APP_BASE_API_URL", process.env.REACT_APP_BASE_API_URL);
+console.log("What envs do we have", process.env);
 export const client = new ApolloClient({
     cache,
-    link: new HttpLink({ uri: `${process.env.REACT_APP_BASE_API_URL}/graphql` })
+    link: new HttpLink({ uri: process.env.REACT_APP_BASE_API_URL ? `${process.env.REACT_APP_BASE_API_URL}/graphql` : '/graphql' })
 });
