cb0a6caba7ab10bb0d8922af3d51849f06ec06a3
Paul Meyer <paul.meyer@nexenio.com>
2021-02-15 13:49:57 +0100

    prettier fixes (1)

--- src/artist/form/artistForm.tsx
+++ src/artist/form/artistForm.tsx
@@ -1,50 +1,35 @@
 import React from "react";
 import { Paper } from "@material-ui/core";
 
-import FormStepper from "./formStepper";
+import { ArtistBasicInformation, ArtistAdvancedInformation, SocialMediaLinks } from "../artist.model";
 
+import FormStepper from "./formStepper";
 import { FormRoot } from "./styled";
-import {
-	ArtistBasicInformation,
-	ArtistAdvancedInformation,
-	SocialMediaLinks,
-} from "../artist.model";
 
 interface Props {
-	submitButtonLabel: string;
-	hasReset?: boolean;
-	basicInformation?: ArtistBasicInformation;
-	advancedInformation?: ArtistAdvancedInformation;
-	socialMediaLinks?: SocialMediaLinks;
-	onFormSubmit: (
-		basic: ArtistBasicInformation,
-		advanced: ArtistAdvancedInformation,
-		socialMedia: SocialMediaLinks
-	) => void;
+    submitButtonLabel: string;
+    hasReset?: boolean;
+    basicInformation?: ArtistBasicInformation;
+    advancedInformation?: ArtistAdvancedInformation;
+    socialMediaLinks?: SocialMediaLinks;
+    onFormSubmit: (basic: ArtistBasicInformation, advanced: ArtistAdvancedInformation, socialMedia: SocialMediaLinks) => void;
 }
 
 export const ArtistForm = (props: Props) => {
-	const {
-		submitButtonLabel,
-		hasReset = false,
-		onFormSubmit,
-		basicInformation,
-		advancedInformation,
-		socialMediaLinks,
-	} = props;
+    const { submitButtonLabel, hasReset = false, onFormSubmit, basicInformation, advancedInformation, socialMediaLinks } = props;
 
-	return (
-		<Paper>
-			<FormRoot autoComplete="off">
-				<FormStepper
-					hasReset={hasReset}
-					submitButtonLabel={submitButtonLabel}
-					handleFormSubmit={onFormSubmit}
-					basicInformation={basicInformation}
-					advancedInformation={advancedInformation}
-					socialMediaLinks={socialMediaLinks}
-				/>
-			</FormRoot>
-		</Paper>
-	);
+    return (
+        <Paper>
+            <FormRoot autoComplete="off">
+                <FormStepper
+                    hasReset={hasReset}
+                    submitButtonLabel={submitButtonLabel}
+                    handleFormSubmit={onFormSubmit}
+                    basicInformation={basicInformation}
+                    advancedInformation={advancedInformation}
+                    socialMediaLinks={socialMediaLinks}
+                />
+            </FormRoot>
+        </Paper>
+    );
 };
