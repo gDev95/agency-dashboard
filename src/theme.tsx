20f3ed4669562a75fde12bad07ff3780ad96f51c
Paul Meyer <paulmeyerber@gmail.com>
2021-05-21 10:35:11 +0200

    add material theme and customize it

---
+++ src/theme.tsx
@@ -0,0 +1,18 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: "#333",
+            main: "#000",
+            dark: "#002884",
+            contrastText: "#fff",
+        },
+        secondary: {
+            light: "#ff7961",
+            main: "#F3B613",
+            dark: "#FFF",
+            contrastText: "#000",
+        },
+    },
+});
